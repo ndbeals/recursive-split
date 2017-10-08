2cf4993c7754cd47a03215e0bda0bc2af2c2c775
Nathan Beals <nathan_beals@hotmail.com>
2017-10-07 21:31:35 -0400

    Added automated installed
    install.sh is the lazy way so somecan curl | sudo bash to install. evil,
    I know.

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+tmpdir=/tmp/split-"$RANDOM"
+mkdir -p "$tmpdir"
+
+cd "$tmpdir"
+
+if wget -q https://raw.githubusercontent.com/ndbeals/recursive-split/master/split-directory > /dev/null &&
+	wget -q https://raw.githubusercontent.com/ndbeals/recursive-split/master/unsplit-directory > /dev/null
+then
+	chmod +x "$tmpdir"/*
+
+	if mv "$tmpdir"/* /usr/local/bin 2> /dev/null
+	then
+		echo -e "\e[92mSUCCESS\e[39m: installed recursive-split successfully"
+	else
+		echo -e "\e[91mFAILURE\e[39m: failed to move files in /usr/local/bin"
+	fi
+
+	rm -rf "$tmpdir"
+else
+	echo -e "\e[91mFAILURE\e[39m: failed to download the scripts from github"
+fi
+
+
+
